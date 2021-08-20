9a16763680a5eab6c52ab8bfe281e86fad526586
Dilek Sarıyerlioğlu <32296332+dilek06@users.noreply.github.com>
2021-08-20 12:04:11 +0300

    Add project files.

---
+++ vtUyg/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vtUyg.ViewModel;
+
+namespace vtUyg.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        DilekContext db = new DilekContext();
+
+        public ActionResult Index()
+        {
+            List<kayitModel> kayitListe = db.Kayitlar.ToList();
+            return View(kayitListe);
+        }
+
+        public ActionResult yeniKayit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult yeniKayit(kayitModel model)
+        {
+            kayitModel kayit = new kayitModel();
+            kayit.adsoyad = model.adsoyad;
+            kayit.mail = model.mail;
+            kayit.yas = model.yas;
+
+            db.Kayitlar.Add(kayit);
+            db.SaveChanges();
+
+            ViewBag.sonuc = "Kayıt Yapıldı";
+            return View();
+        }
+
+        public ActionResult kayitDuzenle(int? id)
+        {
+            kayitModel kayit = db.Kayitlar.Where(k => k.kayId == id).SingleOrDefault();
+            kayitModel model = new kayitModel();
+            model.kayId = kayit.kayId;
+            model.adsoyad = kayit.adsoyad;
+            model.mail = kayit.mail;
+            model.yas = kayit.yas;
+
+            return View(model);
+        }
+
+        public ActionResult kayitSil(int? id)
+        {
+            kayitModel kayit = db.Kayitlar.Where(k => k.kayId == id).SingleOrDefault();
+            db.Kayitlar.Remove(kayit);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult kayitDuzenle(kayitModel m)
+        {
+            kayitModel kayit = db.Kayitlar.Where(k => k.kayId == m.kayId).SingleOrDefault();
+            kayit.adsoyad = m.adsoyad;
+            kayit.mail = m.mail;
+            kayit.yas = m.yas;
+            db.SaveChanges();
+
+            ViewBag.sonuc = "Kayıt Güncellendi";
+            return View();
+        }
+
+    }
+}
---
+++ vtUyg/ViewModel/DilekContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace vtUyg.ViewModel
+{
+    public class DilekContext : DbContext
+    {
+        public DilekContext()
+        {
+            Database.Connection.ConnectionString = "Data Source=.;Initial Catalog=DilekCodeFirst;Integrated Security=True";
+        }
+
+        public DbSet<kayitModel> Kayitlar { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
